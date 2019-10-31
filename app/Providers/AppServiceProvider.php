8ea5233c0ca24c2f1784b6d6d86631409b1a3b4b
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-10-31 19:28:20 +0700

    force to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(100);
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
